68b9be47829b511d54615a1b6d18e07fc055f6a1
Emmanuel Omene <e.omene2012@gmail.com>
2023-05-08 13:24:38 +0200

    Update TimingSystem/Runtime/TimingSystem/Scripts/TestTimer.cs

---
+++ TimingSystem/Runtime/TimingSystem/Scripts/TestTimer.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+using TimingSystem.Runtime.TimingSystem.Scripts;
+
+public class TestTimer : AbstractTimer
+{
+    protected override void OnComplete()
+    {
+        // Do nothing for the test timer
+    }
+}
