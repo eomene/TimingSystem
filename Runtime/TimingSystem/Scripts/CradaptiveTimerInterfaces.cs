be3a1510dc6badb81f14517e64f648af69bb4d33
Emmanuel Omene <e.omene2012@gmail.com>
2023-05-10 16:20:10 +0200

    Update Runtime/TimingSystem/Scripts/CradaptiveTimerInterfaces.cs

--- Runtime/TimingSystem/Scripts/CradaptiveTimerInterfaces.cs
+++ Runtime/TimingSystem/Scripts/CradaptiveTimerInterfaces.cs
@@ -1,12 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-namespace Cradaptive.AbstractTimer
+
+/// <summary>
+/// Interface representing the functions of a Cradaptive timer.
+/// </summary>
+public interface ICradaptiveTimer
 {
-    public interface IAbstractTimerConsumer
-    {
-        void onTimerUpdated(CradaptiveTimerClass timerValue);
-        void onTimerEnded();
-    }
+    /// <summary>
+    /// Initialize the timer with the given parameters.
+    /// </summary>
+    /// <param name="timerData">CradaptiveTimerData object to set up the base duration and duration modifier.</param>
+    /// <param name="onTimerComplete">Action to perform when the timer completes.</param>
+    void Initialize(CradaptiveTimerData timerData, Action onTimerComplete);
+
+    /// <summary>
+    /// Modify the duration of the timer using a specified factor.
+    /// </summary>
+    /// <param name="factor">Factor by which the duration will be modified.</param>
+    void ModifyDuration(float factor);
+}
+
+/// <summary>
+/// Interface representing the functions of a Cradaptive timer provider.
+/// </summary>
+public interface ICradaptiveTimerProvider
+{
+    /// <summary>
+    /// Get a Cradaptive timer with the specified timer data.
+    /// </summary>
+    /// <param name="timerData">CradaptiveTimerData object to create timer from.</param>
+    /// <returns>New instance of a Cradaptive timer with the given timer data.</returns>
+    ICradaptiveTimer GetCradaptiveTimer(CradaptiveTimerData timerData);
 }
